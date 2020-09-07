0ce81aa12e12d59c248315935b9a595c09b3e48d
nguyentai3021 <nguyentai3021@gmail.com>
2020-09-07 10:39:12 +0700

    Create entity classes and setup EF Core

---
+++ eShopSolution.Data/EF/EShopDBContext.cs
@@ -0,0 +1,20 @@
+﻿using eShopSolution.Data.Entity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace eShopSolution.Data.EF
+{
+    public class EShopDBContext : DbContext
+    {
+        public EShopDBContext([NotNullAttribute] DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Product> Products;
+        public DbSet<Category> Categories;
+    }
+}
