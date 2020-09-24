438b9100b941835dcdfbcfa0465dbb479ec49543
tainguyen3021 <nguyentai3021@gmail.com>
2020-09-24 10:47:41 +0700

    DTOS

---
+++ eShopSolution.Application/Catalog/Products/ManageProductService.cs
@@ -0,0 +1,42 @@
+﻿using eShopSolution.Application.Catalog.Products.DTO;
+using eShopSolution.Application.Catalog.Products.DTOs;
+using eShopSolution.Data.EF;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Application.Catalog.Products
+{
+    public class ManageProductService : IManageProductService
+    {
+        private readonly EShopDbContext _context;
+        public ManageProductService(EShopDbContext context)
+        {
+            _context = context;
+        }
+        int IManageProductService.Create(ProductCreateRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        int IManageProductService.Delete(int ProductId)
+        {
+            throw new NotImplementedException();
+        }
+
+        List<ProductViewModel> IManageProductService.GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        List<ProductViewModel> IManageProductService.GetAllPaging(string keyword, int pageIndex, int pageSize)
+        {
+            throw new NotImplementedException();
+        }
+
+        int IManageProductService.Update(ProductCreateRequest request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
